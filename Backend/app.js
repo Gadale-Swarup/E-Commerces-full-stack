b8dd3058ee59c297d187c5fe7dca5ebe91b3ce75
Gadale-Swarup <swarup.g1011@gmail.com>
2024-08-13 22:41:42 +0530

    New

---
+++ Backend/app.js
@@ -0,0 +1,31 @@
+const express=require('express');
+const mongoose=require('mongoose');
+const bodyParser=require('body-parser');
+const productRoutes=require('./routes/productRoute');
+const categoryRoutes = require('./routes/categoryRoutes');
+const userRoutes=require('./routes/userRoute');
+require('dotenv').config();
+
+const app=express();
+const port=process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.once('open',()=>{
+    console.log('connected to MongoDB database');
+});
+
+
+//userRoute
+app.use('/api/users',userRoutes);
+
+//CategoryRoute 
+app.use('/api/categories',categoryRoutes);
+
+// ProductsRoute
+app.use('/api/products',productRoutes);
+
+app.listen(port,()=>{
+    console.log(`server is running on port ${port}`);
+});
